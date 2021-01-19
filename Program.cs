3fe1aafb4208bf043e8672719a1f477071164101
PatMag68 <77663660+PatMag68@users.noreply.github.com>
2021-01-19 12:02:35 +0100

    All but error handling done

--- Program.cs
+++ Program.cs
@@ -9,45 +9,50 @@ namespace PatMag_StoneScissorsPaper
 
         static void Main(string[] args)
         {
-
-
             int programScore = 0;
             int playerScore = 0;
             bool keepPlaying = true;
-
 
             Console.WriteLine("Welcome to a game of Scissors, Rock, Paper against the PC.\nFirst to 3 points wins and\nyou will select your hand first.\n");
             while (keepPlaying)
             {
                 while (playerScore < 3 && programScore < 3)
                 {
+                    Random rnd = new Random();
+                    int programRandomHand = rnd.Next(1, 3);
+                    //S = 1, R = 2, P = 3
                     Console.WriteLine("Please select hand:\nPress S for SCISSORS.\nPress R for ROCK.\nPress P for PAPER.");
                     string playerHand = Console.ReadKey().Key.ToString();
-                    string programHand = "P";
-                    Random programHands = new Random();
-                    if (playerHand == "S" && programHand == "R")
+
+                    if (playerHand == "S" && programRandomHand == 2)
                     {
                         programScore++;
+                        Console.WriteLine("\nYou selected Scissors and the program selected Rock.\nYou: " + playerScore + "\nProgram: " + programScore + "\n");
                     }
-                    else if (playerHand == "S" && programHand == "P")
+                    else if (playerHand == "S" && programRandomHand == 3)
                     {
                         playerScore++;
+                        Console.WriteLine("\nYou selected Scissors and the program selected Paper.\nYou: " + playerScore + "\nProgram: " + programScore + "\n");
                     }
-                    else if (playerHand == "R" && programHand == "S")
+                    else if (playerHand == "R" && programRandomHand == 1)
                     {
                         playerScore++;
+                        Console.WriteLine("\nYou selected Rock and the program selected Scissors.\nYou: " + playerScore + "\nProgram: " + programScore + "\n");
                     }
-                    else if (playerHand == "R" && programHand == "P")
+                    else if (playerHand == "R" && programRandomHand == 3)
                     {
                         programScore++;
+                        Console.WriteLine("\nYou selected Rock and the program selected Paper.\nYou: " + playerScore + "\nProgram: " + programScore + "\n");
                     }
-                    else if (playerHand == "P" && programHand == "R")
+                    else if (playerHand == "P" && programRandomHand == 2)
                     {
                         playerScore++;
+                        Console.WriteLine("\nYou selected Paper and the program selected Rock.\nYou: " + playerScore + "\nProgram: " + programScore + "\n");
                     }
-                    else if (playerHand == "P" && programHand == "S")
+                    else if (playerHand == "P" && programRandomHand == 1)
                     {
                         programScore++;
+                        Console.WriteLine("\nYou selected Paper and the program selected Scissors.\nYou: " + playerScore + "\nProgram: " + programScore + "\n");
                     }
                     else
                     {
